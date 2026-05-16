fa7a44854b5bac38cf11f2eaca804589277197f5
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-05-16 15:14:43 +0100

    creating CRUD Frais test 07

--- src/app/api/frais/route.ts
+++ src/app/api/frais/route.ts
@@ -32,7 +32,6 @@ export async function GET(request: Request) {
             .sort({ createdAt: -1 })
             .skip(safeOffset)
             .limit(safeLimit)
-            .populate("programmes")
             .populate("annee")
             .lean();
 
