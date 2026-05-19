5cfe44551726145aa171d9db09bbd981c8b4c3aa
gaida-lisongo <inbtpkinshasa@gmail.com>
2026-05-19 01:02:46 +0100

    creating layout dahsboard organisatuer 06

--- src/components/dashboard/DashboardOrganisateur.tsx
+++ src/components/dashboard/DashboardOrganisateur.tsx
@@ -20,6 +20,28 @@ export default function DashboardOrganisateur(props: DashboardOrganisateurProps)
     console.log("Programmes :", programmes);
     console.log("Charges Horaires :", chargesHoraires);
 
+    const categories : {
+        title: string;
+        amount: number;
+        total: number;
+        icon: string;
+        color: string;
+        rows: any[];
+    }[] = !chargesHoraires ? [] : chargesHoraires.items.map((charge: any) => ({
+        title: `${charge.programmeDesignation}`,
+        amount: (programmes.find((p) => String(p.slug) === String(charge.programmeSlug))?.credits || 0) * 25,
+        total: charge?.count,
+        icon: "mdi:school", //SchoolIcon,
+        color: "blue",
+        rows: charge?.items || [],
+    }));
+
+    const totalCharges = categories.reduce((sum, category) => sum + category.total, 0);
+    const totalVolumeHoraire = categories.reduce((sum, category) => sum + category.amount, 0);
+
+    console.log("Categories :", categories);
+    console.log("Total Charges :", totalCharges);
+    console.log("Total Volume Horaire :", totalVolumeHoraire);
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">Dashboard Organisateur</h1>
